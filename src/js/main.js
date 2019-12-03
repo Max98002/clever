d1912eb04771134db5b7d720eaec673e4b674290
Max98002 <maksimka98002@gmail.com>
2019-12-03 16:48:22 +0200

    add modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
   var menu__button = $('.header-btn');
   var menu = $('.header-menu');
+  var brif = $('.brif');
+  var modal = $('.modal');
+  var close = $('.modal-header__close');
 
   menu__button.on('click', function () {
     menu.toggleClass('header-menu_active');
+  })
+
+  brif.on('click', function () {
+    modal.addClass('modal_active');
+  })
+  close.on('click', function () {
+    modal.removeClass('modal_active');
   })
 });
