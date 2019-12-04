02648d85f653df6302a5968c5b68db621f51a887
Max98002 <maksimka98002@gmail.com>
2019-12-04 18:15:59 +0200

    sd

--- src/js/main.js
+++ src/js/main.js
@@ -41,12 +41,119 @@ $(document).ready(function () {
   $("a[href^='#']").click(function () {
     event.preventDefault();
     var _href = $(this).attr("href");
-    $("html, body").animate({ scrollTop: $(_href).offset().top+ -180 + "px" }, 800);
+    $("html, body").animate({ scrollTop: $(_href).offset().top + -180 + "px" }, 800);
     return false;
   });
   // Прокрутка До секцій
   var link = $('.link');
-  link.on('click', function(){
+  link.on('click', function () {
     menu.removeClass('header-menu_active');
   })
+
+  // Валідація форми
+  $('#form-contact').validate({
+    rules: {
+      username: {
+        required: true,
+        minlength: 2,
+        maxlength: 8,
+      },
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    errorElement: "div",
+    errorClass: "error",
+    messages: {
+      username: {
+        required: "Вкажіть ім'я",
+        minlength: jQuery.validator.format("Залишилось {0} символи"),
+        maxlength: jQuery.validator.format("Не більше {0} символів")
+      },
+      email: {
+        required: "Заповніть поле Email",
+        email: "Введіть коректний Email"
+      }
+    },
+    // submitHandler: function (form) {
+    //   $.ajax({
+    //     url: form.action,
+    //     type: form.method,
+    //     data: $(form).serialize(),
+    //     success: function (response) {
+    //       console.log("Hello Все гуд: " + response);
+    //       $('.modal-form')[0].reset();
+    //       modal.removeClass('modal-active');
+    //       img_load.removeClass('modal__button_image-active');
+    //       var thanks = $('#thanks');
+    //       thanks.addClass('thanks-active');
+    //       function close() {
+    //         thanks.removeClass('thanks-active');
+    //       }
+    //       setTimeout(close, 3000);
+    //     },
+    //     error: function (jqHXR, textStatus, errorThrown) {
+    //       console.log(jqHXR + " " + textStatus);
+    //     }
+    //   });
+    // }
+  });
+  $('.modal-form').validate({
+    rules: {
+      username: {
+        required: true,
+        minlength: 2,
+        maxlength: 8,
+      },
+      namecompany: {
+        required: true,
+        maxlength: 12,
+      },
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    errorElement: "div",
+    errorClass: "error",
+    messages: {
+      username: {
+        required: "Вкажіть ім'я",
+        minlength: jQuery.validator.format("Залишилось {0} символи"),
+        maxlength: jQuery.validator.format("Не більше {0} символів")
+      },
+      namecompany: {
+        required: "Вкажіть назву компанії",
+        maxlength: jQuery.validator.format("Не більше {0} символів")
+      },
+      email: {
+        required: "Заповніть поле Email",
+        email: "Введіть коректний Email"
+      }
+    },
+    // submitHandler: function (form) {
+    //   $.ajax({
+    //     url: form.action,
+    //     type: form.method,
+    //     data: $(form).serialize(),
+    //     success: function (response) {
+    //       console.log("Hello Все гуд: " + response);
+    //       $('.modal-form')[0].reset();
+    //       modal.removeClass('modal-active');
+    //       img_load.removeClass('modal__button_image-active');
+    //       var thanks = $('#thanks');
+    //       thanks.addClass('thanks-active');
+    //       function close() {
+    //         thanks.removeClass('thanks-active');
+    //       }
+    //       setTimeout(close, 3000);
+    //     },
+    //     error: function (jqHXR, textStatus, errorThrown) {
+    //       console.log(jqHXR + " " + textStatus);
+    //     }
+    //   });
+    // }
+  });
+  // Валідація форми
 });
